d6323490514d7989d388d902a10ba9414d478d40
Tarociarz-z-Macedonii <olafwszeborowski123@gmail.com>
2025-05-05 11:47:34 +0200

    asa

--- scripts/room_generator.gd
+++ scripts/room_generator.gd
@@ -7,16 +7,15 @@ var room_height: int =  room_px_height * 16
 var offset_x: int = 4 * room_width
 var offset_y: int = 4 * room_height 
 
-var room_cords = []
 var rooms = {} 
 var end_rooms = []
-var rooms_to_create_queue = []
-var max_num_rooms: int = 20
-var min_num_rooms: int  = 30
+var cords_to_create_neighbours = []
+var max_num_rooms: int = 20 #inclusive
+var min_num_rooms: int  = 20 #inclusive
 var chance_for_room: float  = 0.5
 var max_neighbours: int  = 1
 var room_level: int  = 1
-var sum_of_rooms = 0
+var added_neighbours = 0
 
 var finished_creating := false
 var minimap_displayer
@@ -27,15 +26,16 @@ func _ready() -> void:
 	position = Vector2(-offset_x, -offset_y)
 	
 	_create_room(Vector2(4,4))
-	_try_to_add_to_neighbours(Vector2(4,4)) 
+	cords_to_create_neighbours.append(Vector2(4,4))
 
 func _process(_delta: float) -> void:
-	if len(rooms_to_create_queue) > 0:
-		var cords = rooms_to_create_queue.pop_front()
+	if len(cords_to_create_neighbours) > 0:
+		var cords = cords_to_create_neighbours.pop_front()
 		_try_to_add_to_neighbours(cords) 
 		return
 		
-	on_finished_creating()	
+	if not finished_creating:
+		on_finished_creating()	
 
 func _create_room(cords):
 	var temp_room = room.instantiate()
@@ -43,29 +43,28 @@ func _create_room(cords):
 	add_child(temp_room)
 
 func _try_to_add_to_neighbours(cords):
-	var added_neighbours = 0
-	if len(rooms) < max_num_rooms:
-		if cords.y - 1 >= 0:
-			if _try_to_add_to_neighbour(Vector2(cords.x, cords.y - 1)):
-				added_neighbours += 1
-		if cords.y + 1 <= 9:
-			if _try_to_add_to_neighbour(Vector2(cords.x, cords.y + 1)):
-				added_neighbours += 1
-		if cords.x - 1 >= 0:
-			if _try_to_add_to_neighbour(Vector2(cords.x - 1, cords.y )):
-				added_neighbours += 1
-		if cords.x + 1 <= 9:
-			if _try_to_add_to_neighbour(Vector2(cords.x + 1, cords.y )):
-				added_neighbours += 1
+	print(cords)
+	print(finished_creating)
+	added_neighbours = 0
+	if cords.y - 1 >= 0:
+		_try_to_add_to_neighbour(Vector2(cords.x, cords.y - 1))
+	if cords.y + 1 <= 9:
+		_try_to_add_to_neighbour(Vector2(cords.x, cords.y + 1))
+	if cords.x - 1 >= 0:
+		_try_to_add_to_neighbour(Vector2(cords.x - 1, cords.y ))
+	if cords.x + 1 <= 9:
+		_try_to_add_to_neighbour(Vector2(cords.x + 1, cords.y ))
 	if added_neighbours == 0:
 		end_rooms.push_back(cords)
 
 func _try_to_add_to_neighbour(cords):
-	if randf_range(0,1.0) < chance_for_room and not is_room_existing(cords) and _count_neighbours(cords) <= max_neighbours:
-		rooms_to_create_queue.push_back(cords)
+	if (randf_range(0,1.0) < chance_for_room 
+	and not is_room_existing(cords) 
+	and _count_neighbours(cords) <= max_neighbours 
+	and len(rooms) < max_num_rooms):
+		cords_to_create_neighbours.push_back(cords)
 		_create_room(cords)
-		return true
-	return false
+		added_neighbours += 1
 
 func is_room_existing(cords):
 	for key in rooms:
@@ -86,35 +85,33 @@ func _count_neighbours(cords):
 	return neighbours
 
 func on_finished_creating():
-	if not finished_creating:
-		finished_creating = true
-		#print(len(rooms))
-		if len(rooms) < min_num_rooms or len(end_rooms) < 2:
-			restart()
-			return
-		_assign_final_room_values()
-		minimap_displayer.display_minimap(rooms)
+	if len(rooms) < min_num_rooms or len(end_rooms) < 2:
+		restart()
+		return
+	finished_creating = true
+	_assign_final_room_values()
+	minimap_displayer.display_minimap(rooms)
 
 func restart():
-	sum_of_rooms += len(rooms)
-	print('restarted: ' + str(sum_of_rooms))
+	print("restarted: " + str(len(rooms)))
 	rooms.clear()
 	end_rooms.clear()
 	_create_room(Vector2(4,4))
-	_try_to_add_to_neighbours(Vector2(4,4)) 
-	finished_creating = false
+	cords_to_create_neighbours.append(Vector2(4,4))
 	
 func _assign_final_room_values():
+	print("Rooms: " + str(len(rooms)) + " EndRooms: " + str(len(end_rooms)))
+	
+	for key in rooms:
+		rooms[key].type = Enums.RoomType.ENEMY
+	rooms[end_rooms.pop_back()].type = Enums.RoomType.BOSS
+	rooms[end_rooms.pop_back()].type = Enums.RoomType.CHEST
+	
 	for key in rooms:
 		rooms[key].cords = key
 		rooms[key].status = "unexplored"
-		rooms[key].type = Enums.RoomType.ENEMY
 		rooms[key].level = room_level
 		rooms[key].position = Vector2(key.x * room_width, key.y * room_height) 
-	print("EndRooms: " + str(len(end_rooms)))
-	rooms[end_rooms.pop_back()].type = Enums.RoomType.BOSS
-	rooms[end_rooms.pop_back()].type = Enums.RoomType.CHEST
-	for key in rooms:
 		rooms[key].instantiate_interior()
 		_assign_openings(key, rooms[key])
 		rooms[key].make_corridors()
